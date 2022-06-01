649bb43fd1722c81a58a01f27c84f7a2e74e6227
Alvi-soltakhanov <Alvi-soltakanov@mail.ru>
2022-06-02 00:10:25 +0300

    Updates

---
+++ precache-manifest.eb5a5b6952a4a70b1ff7857018cb570d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09f50cea8ac746d5d190",
+    "url": "/frontend-uchi/static/css/main.f6c73056.chunk.css"
+  },
+  {
+    "revision": "09f50cea8ac746d5d190",
+    "url": "/frontend-uchi/static/js/main.09f50cea.chunk.js"
+  },
+  {
+    "revision": "43d866972be04782ec9c",
+    "url": "/frontend-uchi/static/js/1.43d86697.chunk.js"
+  },
+  {
+    "revision": "bbfc9886764c9d7965e1",
+    "url": "/frontend-uchi/static/js/runtime~main.bbfc9886.js"
+  },
+  {
+    "revision": "cb0bd731e397317cafca8bb82d44b28a",
+    "url": "/frontend-uchi/index.html"
+  }
+];
